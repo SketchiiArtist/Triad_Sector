51fe4204f362f5bbcd3b4292daaac4ba1ca253e9
SketchiiArtist <artistsketchy@gmail.com>
2026-05-30 11:04:00 -0400

    Extended char limit for descriptions by 8x

--- Content.Shared/Preferences/HumanoidCharacterProfile.cs
+++ Content.Shared/Preferences/HumanoidCharacterProfile.cs
@@ -32,7 +32,7 @@ namespace Content.Shared.Preferences
 
         public const int MaxNameLength = 32;
         public const int MaxLoadoutNameLength = 32;
-        public const int MaxDescLength = 512;
+        public const int MaxDescLength = 4096;
 
         public const int DefaultBalance = 75000;
 
