bab6bb5f67fc181b24e415e8f5c58418d70891bf
SketchiiArtist <artistsketchy@gmail.com>
2026-06-06 13:23:12 -0400

    Ported Showers

---
+++ Content.Server/_Den/Shower/ShowerSystem.cs
@@ -0,0 +1,9 @@
+/// Made by Sleepy Scarecrow for Denstation
+using Content.Shared._Den.Showers;
+
+namespace Content.Server._Den.Shower;
+
+public sealed class ShowerSystem : SharedShowerSystem
+{
+
+}
